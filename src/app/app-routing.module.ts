4dd00156bdbfcdf9fb163cb7686891acb5670b4c
hohjanna <ardilayoki@gmail.com>
2021-07-12 12:52:01 -0500

    grid html added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { WaiterComponent } from './components/waiter/waiter.component';
+import { ChefComponent } from './components/chef/chef.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'waiter', component: WaiterComponent},
+  { path: 'chef', component: ChefComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
